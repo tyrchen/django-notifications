967a07449dc399daa11d362116e210f8e9d93e7d
tchen <tchen@tukeq.com>
2012-09-27 17:35:48 +0800

    revise admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -4,6 +4,15 @@ from django.contrib import admin
 from notifications.models import Notification
 
 class NotificationAdmin(admin.ModelAdmin):
-    pass
+  list_display = ('recipient', 'actor', 'verb', 'description', 'target', 'action_object', 'timestamp', 'readed', 'public')
+  list_filter = ('recipient', 'actor', 'verb')
 
+  def actor(self, obj):
+    return obj.actor
+
+  def target(self, obj):
+    return obj.target
+
+  def action_object(self, obj):
+    return obj.action_object
 admin.site.register(Notification, NotificationAdmin)
